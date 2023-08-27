bc159fbf25effe91245532aa907e6658c3bd76bc
Jorge L. Martinez <jorelmb@gmail.com>
2023-08-27 09:46:48 +0200

    Made base TUI

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,3 +1,9 @@
-pub fn main(){
-    println!("Hello from App!");
+use std::error::Error;
+use rust_gpt::tui::Application;
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn Error>>{
+    // Create application and run
+    let mut app = Application::default();
+    Ok(app.run().await?)
 }
